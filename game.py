205c7e7c9858f07369e67fc6b3cf74418e46203a
hirokiden <hd1043@stern.nyu.edu>
2019-06-05 18:56:17 -0400

    Capture inputs

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 # game.py
 
-print("Rock, Paper, Scissors, Shoot!")
+# print("Rock, Paper, Scissors, Shoot!")
 
 # Activate the following python
 
@@ -10,3 +10,22 @@ print("Rock, Paper, Scissors, Shoot!")
 # pip install pytest  --> install pytest package
 
 # python game.py --> command line
+
+
+
+
+
+# CAPTURE INPUTS
+
+
+user_choice = input("Please choose one of the following: 'rock', 'paper', or 'scissor'")
+
+print("USER CHOICE IS:", user_choice)
+
+# VALIDATE INPUTS
+
+# GENERATE COMPUTER SELECTION
+
+# DETERMINE THE WINNER
+
+# DISPLAY FINAL OUTPUTS / OUTCOMES
